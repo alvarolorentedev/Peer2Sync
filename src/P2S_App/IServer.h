ae5694af50963ed8e05139e3b68f827d1cb3fc70
kanekotic <alvarojosepl@gmail.com>
2016-07-12 09:48:43 +0200

    created abstraction for HTTP methods

--- src/P2S_App/IServer.h
+++ src/P2S_App/IServer.h
@@ -5,10 +5,15 @@
 namespace P2S {
 namespace App {
 
+enum class HTTPMethod
+{
+    POST
+};
+
 class IServer
 {
 public:
-    virtual void Subscribe(const std::string& path, std::function<void()>& function) = 0;
+    virtual void Subscribe(const std::string& path, const HTTPMethod& method,const std::function<void()>& func) = 0;
     virtual void Start(const int& port) = 0;
     virtual ~IServer() = default;
 };
