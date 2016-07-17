2a071dc1e2a9b271d9c2557138a8bc69bceb410f
kanekotic <alvarojosepl@gmail.com>
2016-07-17 20:56:14 +0200

    set rpc path to dummy code response

--- src/P2S_App/main.cpp
+++ src/P2S_App/main.cpp
@@ -1,13 +1,17 @@
 #include "CrowServer.h"
 #include "IServer.h"
+#include <jsonrpcresponse.h>
 
 using namespace std;
 using namespace P2S::App;
+using namespace P2S::Lib;
 
 int main(/*int argc, char *argv[]*/)
 {
-    //std::unique_ptr<IServer> server = std::make_unique<CrowServer>();
-    //server->Subscribe("/", HTTPMethod::POST, [](){});
-    //server->Start(8080);
+    std::unique_ptr<IServer> server = std::make_unique<CrowServer>();
+    server->Subscribe("/rpc", HTTPMethod::POST, [&](const string& req){
+        return make_shared<ValidJsonRpcResponse>("TODO",10);
+    });
+    server->Start(8080);
 }
 
