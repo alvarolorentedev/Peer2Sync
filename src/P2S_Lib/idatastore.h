5af7a7a91e1957a7fc363bf0cbb127159015f3bc
kanekotic <alvarojosepl@gmail.com>
2016-07-29 08:39:38 +0200

    added connection basic interface for database connection

---
+++ src/P2S_Lib/idatastore.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "p2slib.h"
+
+namespace P2S {
+namespace Lib {
+
+/**
+ * @name    IDataStore
+ * @brief   Interface abstraction for database management
+ */
+class P2S_LIBSHARED_EXPORT IDataStore
+{
+public:
+    virtual void Connect(const std::string& host, const uint16_t& port);
+    virtual void Set(std::string key, std::string value) = 0;
+    virtual std::string Get(std::string key) = 0;
+    virtual ~IDataStore() = default;
+};
+
+using IDataStorePtr = std::shared_ptr<IDataStore>;
+
+}
+}
