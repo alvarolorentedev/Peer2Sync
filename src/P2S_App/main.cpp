e4bbe147b6c71795cfaa80732d0c9b1a22acb118
kanekotic <alvarojosepl@gmail.com>
2016-08-16 19:13:01 +0200

    added parameters to command line

--- src/P2S_App/main.cpp
+++ src/P2S_App/main.cpp
@@ -12,12 +12,12 @@ using namespace P2S::Lib;
  * @name    Main
  * @brief   Entrypoit of application
  */
-int main(/*int argc, char *argv[]*/)
+int main(int, char *argv[])
 {
     auto server = make_shared<CrowServer>();
     auto dataStore = make_shared<RedisDataStore>();
-    dataStore->Connect("127.0.0.1", 6379);
+    dataStore->Connect(argv[1], atoi(argv[3]));
     RpcMethods methods(server, dataStore);
-    server->Start(8080);
+    server->Start(atoi(argv[0]));
 }
 
