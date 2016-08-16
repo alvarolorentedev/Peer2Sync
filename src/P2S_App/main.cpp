93394c18ec7991610e787681b3b38b8ba1b10bb4
kanekotic <alvarojosepl@gmail.com>
2016-08-16 19:26:50 +0200

    moved parameters to enviroment variables

--- src/P2S_App/main.cpp
+++ src/P2S_App/main.cpp
@@ -12,12 +12,12 @@ using namespace P2S::Lib;
  * @name    Main
  * @brief   Entrypoit of application
  */
-int main(int, char *argv[])
+int main(/*int, char *argv[]*/)
 {
     auto server = make_shared<CrowServer>();
     auto dataStore = make_shared<RedisDataStore>();
-    dataStore->Connect(argv[1], atoi(argv[3]));
+    dataStore->Connect(getenv("REDIS_IP"), atoi(getenv("REDIS_PORT")));
     RpcMethods methods(server, dataStore);
-    server->Start(atoi(argv[0]));
+    server->Start(atoi(getenv("PORT")));
 }
 
