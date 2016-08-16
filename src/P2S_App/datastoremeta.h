e1fb7d85cb0231d375b816613efa869ee9d6bdf3
kanekotic <alvarojosepl@gmail.com>
2016-08-16 18:32:58 +0200

    added datastoreMeta method to RPC

---
+++ src/P2S_App/datastoremeta.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <idatastore.h>
+#include "irpcmethod.h"
+
+namespace P2S {
+namespace App {
+/**
+ * @name    DataStoreMeta
+ * @brief   Meta method from RPC, it retrieves the array of ids and time changed
+ */
+class DataStoreMeta : public IRpcMethod
+{
+    P2S::Lib::IDataStorePtr datastore;
+public:
+    DataStoreMeta(const P2S::Lib::IDataStorePtr& dstore);
+    virtual Lib::IResponsePtr Execute(const nlohmann::json& params);
+};
+
+}
+}
